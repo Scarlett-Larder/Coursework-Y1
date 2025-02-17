5f2e5f393a4bcc832d209fbd1691243c4acf59b5
ScarlettLarder <scarlett.larder943@gmail.com>
2025-02-17 14:47:50 +0000

    meow

--- Term_2/pract12/circle_info_gui.py
+++ Term_2/pract12/circle_info_gui.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Frame, Label, Entry, IntVar, Button, StringVar
-
+import math
 class CircleInfo:
 
     def __init__(self):
@@ -23,12 +23,45 @@ class CircleInfo:
     
     def create_widgets(self):
         entry_num1 = Entry(
-            self.man_frame,
-            width=
+            self.main_frame,
+            width=20,
+            textvariable=self.num1
         )
+        entry_num1.pack()
 
-def area_of_circle(radius):
-    return math.pi * radius ** 2
+        label_result1 = Label(
+            self.main_frame,
+            width=20,
+            textvariable=self.result1
+        )
+        label_result1.pack()
 
-def circumference_of_circle(radius):
-    return 2 * math.pi * radius
+        label_result2 = Label(
+            self.main_frame,
+            width=20,
+            textvariable=self.result2
+        )
+        label_result2.pack()
+
+        button_calc = Button(
+            self.main_frame,
+            text="calc",
+            compand=self.calculate
+        )
+        button_calc.pack()
+    
+    def calculate(self):
+        self.num1.get()
+        result1 = math.pi * self.num1 ** 2
+        result2 = 2 * math.pi * self.num1
+
+        self.result1.set(f"Area: {result1}")
+        self.result2.set(f"Circ: {result2}")
+
+def main():
+    calc = CircleInfo()
+    calc.run()
+
+
+main()
+
